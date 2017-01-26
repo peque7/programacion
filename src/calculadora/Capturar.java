6b6bf3bf1f1deca126b73327b5c9d2ac3e437a26
jesus zatarain <jesus.517.zatarain@gmail.com>
2017-01-26 10:33:19 -0700

    subiendo primer cambio

---
+++ src/calculadora/Capturar.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author pequesillo
+ */
+public class Capturar {
+    
+    public String ingresaCaracter(String caracter){
+        String value="";
+        
+        for (int i = 0; i < caracter.length(); i++) {
+            if(.isNumeric(caracter.charAt(i)))
+        }
+        
+        return value;
+    }
+}
